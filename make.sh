99ea2fee5b88aa3925a957c01ce674b43fb7e472
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-08-14 15:53:05 +0200

    update jmlr paper in thesis

--- make.sh
+++ make.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-rubber -I itm/images -I mil/images -I partial_supervision -I mil -I exact_learning -I pystruct main.tex
+rubber -c 'setlist arguments --shell-escape'  -I itm/images -I mil/images -I partial_supervision -I mil -I exact_learning -I pystruct --pdf main.tex
